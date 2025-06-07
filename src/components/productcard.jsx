0b527c7f5938fc674b176a0322b019b826fff7d3
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-06-07 12:22:55 +0530

    prac demo2

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import React from 'react'
+import ViewProductButton from '../components/button';
+
+const productcard = () => {
+    const image = "https://magicstudio.com/blog/content/images/2023/10/props-product-photography.webp";
+    const name="sai mani"
+    const price = 1000;
+    
+  return (
+    <div>
+        <img src={image} alt="product" />
+        <h2>Name: {name}</h2>
+        <h2>Price: {price}</h2>
+        <ViewProductButton />
+    </div>
+  )
+}
+
+export default productcard
